54ee3de21e611bde831cdc55d549a9bbf3bdc6d8
nut1414 <nut.pannapat@gmail.com>
2022-05-04 12:04:59 +0700

    add: addFollower, classRegister api endpoint add: middleware lib add: db connection

---
+++ models/Follower.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const followerSchema = new mongoose.Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true },
+  detail: { type: String, default: '' }
+})
+
+export default mongoose.models.Follower || mongoose.model('Follower',followerSchema)
