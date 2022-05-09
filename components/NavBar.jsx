7cf46a2cbfd9fedda919695fed5fb96cce09c0c0
WTFPUn <punn_namw@hotmail.com>
2022-05-09 13:32:17 +0700

    fix: reduce size of button on navbar morefix: change aboutus page name(3)

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -19,17 +19,17 @@ const NavBar = ({page}) => {
 
   return (
     <>
-      <div className="bg-[#242424] flex flex-row font-normal text-xl place-content-center mobile:hidden drop-shadow-md big-device:px-64  px-12 rounded-b-sm round items-center py-8 transition-all duration-500 ease-in-out ">
+      <div className="bg-[#242424] flex flex-row text-xl place-content-center mobile:hidden drop-shadow-md big-device:px-64 font-light px-12 rounded-b-sm round mobile:w-full items-center py-4 transition-all duration-500 ease-in-out ">
       <Logo/>
         <div className={`ml-16 ${page === 1 ? 'text-[#0080FF]' : 'text-white'}`}><Link href="/"><a>Home</a></Link></div>
         <div className={`ml-16 ${page === 2 ? 'text-[#0080FF]' : 'text-white'}`}><Link href="/extraclass" ><a>Extra class</a></Link></div>
-        <div className={`ml-16 ${page === 3 ? 'text-[#0080FF]' : 'text-white'}`}><Link href="/aboutme"><a>About us</a></Link></div>
+        <div className={`ml-16 ${page === 3 ? 'text-[#0080FF]' : 'text-white'}`}><Link href="/aboutus"><a>About us</a></Link></div>
         <button onClick={btclick} className="ml-auto border-2 border-white text-white p-3"><a href="#regisForm">ลงทะเบียนให้ความสนใจ</a></button>
       </div>
-      <div className="bg-[#242424] relative flex font-normal text-xl  justify-between drop-shadow-md medium-tablet:hidden big-device:hidden big-device:mx-64 px-6 rounded-b-sm round items-center	py-8 transition-all duration-500 ease-in-out ">
+      <div className="bg-[#242424] relative flex font-normal text-xl justify-between drop-shadow-md medium-tablet:hidden big-device:hidden  px-2 rounded-b-sm round items-center	py-8 transition-all duration-500 ease-in-out ">
         <MobileBurger page={page}/>
         <Logo/>
-        <button id="interest" onClick={btclick} className=" text-sm border-2 border-white text-white p-2"><a href="#regisForm">ลงทะเบียนให้ความสนใจ</a></button>
+        <button id="interest" onClick={btclick} className=" text-[0.6rem] border-2 border-white text-white p-2  mobile:py-1"><a href="#regisForm">ลงทะเบียนให้ความสนใจ</a></button>
       </div>
     </>
     
