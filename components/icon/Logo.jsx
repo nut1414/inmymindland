6a6e1cf5a10a37532a4b2a2cd18c617098cd9222
WTFPUn <punn_namw@hotmail.com>
2022-05-09 15:06:59 +0700

    fix: navbar responsive for tablet size

--- components/icon/Logo.jsx
+++ components/icon/Logo.jsx
@@ -3,7 +3,7 @@ const Logo = () => {
     <svg
 
       id="icon"
-      className="fill-white mobile:absolute left-1/2"
+      className="fill-white mobile:absolute medium-tablet:absolute left-1/2"
       width="45"
       height="38"
       viewBox="0 0 45 38"
