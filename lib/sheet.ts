367411248a947ab008ba64fd7e49890a759c14bd
WTFPUn <punn_namw@hotmail.com>
2022-06-27 16:46:14 +0700

    update backend js to ts

--- lib/sheet.ts
+++ lib/sheet.ts
@@ -1,15 +1,21 @@
-import { google } from 'googleapis'
+import { GoogleAuth, GoogleAuthOptions, JSONClient } from 'google-auth-library/build/src/auth/googleauth'
+import { google, sheets_v4 } from 'googleapis'
+
+declare global {
+  var ggAuth: any
+  var sheetInstance: any
+}
 
 global.ggAuth = global.ggAuth || new google.auth.GoogleAuth
-global.ggAuth.jsonContent = JSON.parse(process.env.GOOGLE_CRED)
+global.ggAuth.jsonContent = JSON.parse(process.env.GOOGLE_CRED || '{}')
 global.ggAuth.scopes = ['https://www.googleapis.com/auth/spreadsheets']
 
-global.sheetInstance = global.sheetInstance || google.sheets({ version: "v4", auth: global.ggauth })
+global.sheetInstance = global.sheetInstance || google.sheets({ version: "v4", auth: (global as any).ggauth })
 
 export let sheetInstance = global.sheetInstance
 export let ggAuth = global.global.ggAuth
 
-export async function appendToSheet(spreadsheetId,range,arrelem){
+export async function appendToSheet(spreadsheetId: string, range: string, arrelem:string[]){
   try{
     const allelem = arrelem.flat()
     await sheetInstance.spreadsheets.values.append({
