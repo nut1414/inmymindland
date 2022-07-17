036b4ef3dfcd4853044ebb91b4735d9cc3efd2a9
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2022-07-17 15:25:33 +0700

    add: limit to joblist query

--- pages/api/joblists/index.ts
+++ pages/api/joblists/index.ts
@@ -16,10 +16,11 @@ interface ListingDbQuery {
 async function handler(req: NextApiRequestWithMiddleware, res: NextApiResponse) {
   const session = (req.session) ? req.session : {} as HydratedSession 
   const user = await UserInfo.findByUserId(session.id)
-  const { page, tags, min, max, s, userid } = req.query
+  const { page, tags, pmin, pmax, s, lim, userid } = req.query
   const pagenum = parseInt(page as string) || 1
-  const pricemin = parseInt(min as string)
-  const pricemax = parseInt(max as string)
+  const limit = parseInt(lim as string) || 25
+  const pricemin = parseInt(pmin as string)
+  const pricemax = parseInt(pmax as string)
   try{
     if (req.method === 'GET'){
       let query: ListingDbQuery = {}
@@ -38,11 +39,14 @@ async function handler(req: NextApiRequestWithMiddleware, res: NextApiResponse) 
         query.price['$gte'] = clamp(pricemin, 0, 20000)
       }
 
-      const joblist = await JobListing.find(query,{},{limit: 25, skip:clamp((pagenum-1)*25, 0, 2000)})
+      const joblist = await JobListing.find(query,{},{limit, skip:clamp((pagenum-1)*limit, 0, 2000)})
       const jobcount = await JobListing.find(query).countDocuments()
       res.status(200).json({
         result: joblist,
-        total: jobcount
+        total: jobcount,
+        page: pagenum,
+        total_pages: Math.ceil(jobcount / limit),
+        limit: limit
       })
     }else if (user.role === 'worker' || 
               user.role === 'admin'){
