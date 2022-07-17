23e6936a2ffe13f641fc090bdc547fe77c74fb64
WTFPUn <punn_namw@hotmail.com>
2022-07-17 15:18:21 +0700

    fix: job card following latest design

--- components/hiring/Job.tsx
+++ components/hiring/Job.tsx
@@ -1,3 +1,4 @@
+import { ShareIcon } from "@heroicons/react/solid";
 import { trusted } from "mongoose";
 import Image from "next/image";
 import Link from "next/link";
@@ -29,30 +30,34 @@ const Job = ({jobInfo}: {jobInfo: IJob}) => {
   let i = 0;
   return (
     <div className="flex flex-col gap-2 ">
-    <div className="text-center box-content h-[11rem] w-[12rem] bg-white text-white pb-8 py-4 rounded-md overflow-hidden transition drop-shadow px-4">
+      <div className="text-center box-content h-[13rem] w-[12rem] bg-white text-white pb-8 py-4 rounded-md overflow-hidden transition drop-shadow px-4 relative">
+        <div className="w-full">
+          <Image src={"/mock/rectangle.png"} width="190" height="100" alt="rectest" layout="fixed" className="rounded-md"/>
+        </div>
         <div className="flex flex-col">
-            <div className="flex flex-col w-3/4">
-              <div className=" text-black text-left text-xl truncate">{jobInfo.name}</div>
-              <div className=" text-black text-left text-base truncate">{jobInfo.description}</div>
-            </div>
-          <div className=" flex w-full my-2 gap-2  overflow-y-hidden overflow-x-scroll">
+          <div className="flex flex-col w-full">
+            <div className=" text-black text-left text-xl truncate">{jobInfo.name}</div>
+            <div className=" text-black text-left text-base truncate">{jobInfo.description}</div>
+          </div>
+          <div className=" flex w-full mt-2 gap-2  overflow-y-hidden overflow-x-scroll">
             {jobInfo.tags?.map((tag: TagInfo) => (
-              <div key={jobInfo.uid+i++} className=" bg-black font-light text-xs flex flex-row rounded-full px-3 py-1 place-items-center">
+              <div key={jobInfo.uid+i++} className=" bg-black font-light text-xs flex flex-row rounded-full px-3 py-1 place-items-center scroll-smooth">
                 <div className={colorTag(tag.name)+' mr-2 text-sm w-3 h-3 rounded-full '}/>
                 {tag.name}
               </div>))}
           </div>
-          <div className="flex flex-row h-full">
-            <div className="flex flex-col w-3/4">
-              <div className="ml-4 text-left text-xl truncate">{jobInfo.name}</div>
-              <div className="ml-4 text-left text-base truncate">{jobInfo.description}</div>
-            </div>
-          <div className="flex flex-col w-1/4">
-            <img src={jobInfo.workerimage} alt={jobInfo.name} width={20} height={20} className="rounded-full place-self-end mt-auto"/>
-            <div className="text-xs text-right mr-4">{'โดย ' + jobInfo.worker}</div>
-            </div>
+          <div className="flex flex-row place-content-en text-black place-items-center">
+            <div className="text-originalBlue text-2xl mr-auto">{"฿" + jobInfo.price}</div>
+            <div className=" text-xs text-right mr-4">{'โดย ' + jobInfo.worker}</div>
+            <Image src={jobInfo.workerimage} alt={jobInfo.name} width={24} height={24} layout="fixed" className="rounded-full select-none  mt-auto"/>
           </div>
         </div>
+        <button className="absolute -top-10 -right-10 h-20 w-20 rounded-full bg-black">
+          <div className="relative w-full h-full">
+            <ShareIcon className="absolute w-5 h-5 left-4 bottom-4"/>
+          </div>
+        </button>
+        
       </div>
       <button className=" place-self-center bg-black text-white w-max rounded-full px-2 text-base">จ้างงาน</button>
       <button className=" place-self-center place-items-center text-base text-originalBlue flex flex-row gap-1">
