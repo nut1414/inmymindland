47c3d1314e296c22d2a0daf3d959190ea5282e83
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2022-07-21 19:40:57 +0700

    fix: protected setting api fix: change post to patch setting api

--- pages/api/setting.ts
+++ pages/api/setting.ts
@@ -9,26 +9,29 @@ import { HydratedSession } from './auth/[...nextauth]'
 async function handler(req: NextApiRequestWithMiddleware, res: NextApiResponse) {
   const session = (req.session) ? req.session : {} as HydratedSession 
   try{
-    if (req.method === 'GET'){
-      let userinfo = await UserInfo.findByUserId(session.id)
-      res.status(200).json({status: 'success', userinfo})
-    }else if (req.method === 'POST'){
-      let userinfo = await UserInfo.findOneAndUpdate({user:session.id},
-        {
-          contact: req.body.contact || {},
-          worker_profile: req.body.worker || {}
-        },
-        { new:true }).projection([
-          (req.body.contact) ? 'contact' : '',
-          (req.body.worker)  ? 'worker_profile'  : ''
-        ]).lean()
-      if (userinfo)
+    if(session.id)
+      if (req.method === 'GET'){
+        let userinfo = await UserInfo.findByUserId(session.id)
         res.status(200).json({status: 'success', userinfo})
-      else
-        throw Error('unknown user')
-    } else {
-      res.status(405).json({status:'error', error: 'method not allowed'})
-    }
+      }else if (req.method === 'PATCH'){
+        let userinfo = await UserInfo.findOneAndUpdate({user:session.id},
+          {
+            contact: req.body.contact || {},
+            worker_profile: req.body.worker_profile || {}
+          },
+          { new:true }).projection([
+            (req.body.contact) ? 'contact' : '',
+            (req.body.worker.worker_profile)  ? 'worker_profile'  : ''
+          ]).lean()
+        if (userinfo)
+          res.status(200).json({status: 'success', userinfo})
+        else
+          throw Error('unknown user')
+      } else {
+        res.status(405).json({status:'error', error: 'method not allowed'})
+      }
+    else 
+      res.status(401).json({status:'error', error: 'unauthorized'})
   }catch (e: unknown){
     console.log(e)
     res.status(500).json({status:'error', error: 'internal server error'})
